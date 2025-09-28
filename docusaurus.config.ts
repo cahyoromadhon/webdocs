1ba5266d355e686d12a42ab3c0e2a2031b6a9461
cahyorom <gantengcahyo27@gmail.com>
2025-09-28 23:11:25 +0700

    New Courses

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Sui Surabaya Workshop',
+  title: 'cahyorom',
   tagline: 'Master Sui Blockchain Development',
   favicon: 'img/favicon.ico',
 
--- sidebars.ts
+++ sidebars.ts
@@ -2,22 +2,105 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
   tutorialSidebar: [
-    'intro',
+    'academy',
     {
       type: 'category',
-      label: 'Coinflip Program',
+      label: '1. Getting Started',
       collapsed: false,
       items: [
-        'coinflip/academy',
-        'coinflip/init',
-        'coinflip/module',
-        'coinflip/struct',
-        'coinflip/functions',
-        'coinflip/deploy',
-        'coinflip/kesimpulan',
+        'Getting Started/Why Move on Sui was Created',
       ],
     },
-    // Tambah kategori lain jika perlu, misalnya Hari 2, Hari 3, dsb.
+    {
+      type: 'category',
+      label: '2. Deploying Your First Hello World Contract on Sui',
+      collapsed: true,
+      items: [
+        'Deploying Your First Hello World Contract on Sui/Set Up the Development Environment',
+        'Deploying Your First Hello World Contract on Sui/Deploy Your First Sui Contract',
+        'Deploying Your First Hello World Contract on Sui/Set Up Sui Wallet',
+      ],
+    },
+    {
+      type: 'category',
+      label: '3. Move on Sui Package Architecture',
+      collapsed: true,
+      items: [
+        'Move on Sui Package Architecture/Understand Move toml and Move lock File',
+        'Move on Sui Package Architecture/Basic Module Structure in Move on Sui - I',
+        'Move on Sui Package Architecture/Basic Module Structure in Move on Sui - II',
+      ],
+    },
+    {
+      type: 'category',
+      label: '4. Move on Sui Basics',
+      collapsed: true,
+      items: [
+        'Move on Sui Basics/How to Declare Variables and Constants',
+        'Move on Sui Basics/What are the Different Data Types',
+        'Move on Sui Basics/How Control Flow Works in Move on Sui',
+      ],
+    },
+    {
+      type: 'category',
+      label: '5. Move on Sui Functions',
+      collapsed: true,
+      items: [
+        'Move on Sui Functions/Functions Basics',
+        'Move on Sui Functions/What are the Different Visibility Specifiers',
+      ],
+    },
+    {
+      type: 'category',
+      label: '6. Move on Sui Types with Abilities',
+      collapsed: true,
+      items: [
+        'Move on Sui Types with Abilities/Custom Types and Abilities',
+      ],
+    },
+    {
+      type: 'category',
+      label: '7. Object in Move on Sui',
+      collapsed: true,
+      items: [
+        'Object in Move on Sui/Basics of Objects in Move on Sui',
+        'Object in Move on Sui/Using Objects in Move',
+        'Object in Move on Sui/Objects Ownership',
+      ],
+    },
+    {
+      type: 'category',
+      label: '8. Back to Hello World',
+      collapsed: true,
+      items: [
+        'Back to Hello World/Understand the Hello World Program',
+      ],
+    },
+    {
+      type: 'category',
+      label: '9. Advanced Concepts in Move on Sui',
+      collapsed: true,
+      items: [
+        'Advanced Concepts in Move on Sui/Object Wrapping',
+        'Advanced Concepts in Move on Sui/Events in Move on Sui',
+      ],
+    },
+    {
+      type: 'category',
+      label: '10. Challenge',
+      collapsed: true,
+      items: [
+        'Challange/Challenge Create a Calculator Contract using Move',
+      ],
+    },
+    {
+      type: 'category',
+      label: '11. Let\'s Wrap It Up',
+      collapsed: true,
+      items: [
+        'Let\'s Wrap It Up/Wrap Up',
+      ],
+    },
   ],
 };
 
